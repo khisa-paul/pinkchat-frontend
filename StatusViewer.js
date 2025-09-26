e1e7ed2676e1ea0e2ddc77e449b94803325d88fe
khisa-paul <khisapaul@gmail.com>
2025-09-26 14:41:04 +0300

    Update StatusViewer.js
    update

--- StatusViewer.js
+++ StatusViewer.js
@@ -1,29 +1,41 @@
 import React, { useEffect, useState } from "react";
+import { fetchStatuses } from "../api";
 
 function StatusViewer({ socket }) {
   const [statuses, setStatuses] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:5000/status")
-      .then((res) => res.json())
-      .then(setStatuses);
+    async function loadStatuses() {
+      try {
+        const data = await fetchStatuses();
+        setStatuses(data);
+      } catch (err) {
+        console.error("Error fetching statuses:", err.message);
+      }
+    }
 
-    socket.on("statusUpdate", () => {
-      fetch("http://localhost:5000/status")
-        .then((res) => res.json())
-        .then(setStatuses);
+    loadStatuses();
+
+    // Listen for live updates from socket.io
+    socket.on("status", (status) => {
+      setStatuses((prev) => [status, ...prev]);
     });
+
+    return () => {
+      socket.off("status");
+    };
   }, [socket]);
 
   return (
     <div className="status-viewer">
-      <h3>Status Updates</h3>
-      {statuses.map((s, i) => (
-        <div key={i}>
-          <p>{s.user}</p>
-          <img src={`http://localhost:5000/${s.filePath}`} alt="status" />
-        </div>
-      ))}
+      <h3>Statuses</h3>
+      <ul>
+        {statuses.map((s, idx) => (
+          <li key={idx}>
+            <strong>{s.user}:</strong> {s.text}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
