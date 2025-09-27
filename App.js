44b6eeab789ccbd426646968b70e0de026bb8bb7
khisa-paul <khisapaul@gmail.com>
2025-09-27 10:38:50 +0300

    Update App.js
    update

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react";
 import io from "socket.io-client";
-import { API_BASE } from "./config";
-import { subscribeToNotifications } from "./notifications";
 import ChatWindow from "./ChatWindow";
 import StatusUploader from "./StatusUploader";
 import StatusViewer from "./StatusViewer";
+import { subscribeToNotifications } from "./notifications";
+import { API_BASE } from "./config";
 import Login from "./Login";
 
+// Connect socket.io to backend
 const socket = io(API_BASE, {
   transports: ["websocket"],
   withCredentials: true,
@@ -27,9 +28,9 @@ function App() {
     <div className="app">
       <header className="header">💖 PinkChat</header>
       <div className="main">
-        <ChatWindow socket={socket} currentUser={user} />
+        <ChatWindow socket={socket} currentUser={user.username} />
         <aside>
-          <StatusUploader socket={socket} currentUser={user} />
+          <StatusUploader socket={socket} currentUser={user.username} />
           <StatusViewer socket={socket} />
         </aside>
       </div>
