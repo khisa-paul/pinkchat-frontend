438dabe895d878957241191692d0753a90193d64
khisa-paul <khisapaul@gmail.com>
2025-09-27 13:14:40 +0300

    Update App.js
    update

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import React, { useState } from "react";
-import Login from "./Login.js";
-import Register from "./Register.js";
-import ForgotPassword from "./ForgotPassword.js";
-import Chat from "./Chat.js";
+function App() {
+  const [page, setPage] = React.useState("login");
+  const [user, setUser] = React.useState(null);
 
-export default function App() {
-  const [page, setPage] = useState("login");
-  const [user, setUser] = useState(null);
-
-  if (user) return <Chat user={user} />;
+  const handleLogin = (userData) => {
+    setUser(userData);
+    setPage("chat");
+  };
 
   return (
     <div className="app">
-      {page === "login" && <Login setPage={setPage} setUser={setUser} />}
-      {page === "register" && <Register setPage={setPage} />}
-      {page === "forgot" && <ForgotPassword setPage={setPage} />}
+      {page === "login" && <Login onLogin={handleLogin} onSwitchPage={setPage} />}
+      {page === "register" && <Register onSwitchPage={setPage} />}
+      {page === "forgot" && <ForgotPassword onSwitchPage={setPage} />}
+      {page === "chat" && <Chat user={user} onLogout={() => setPage("login")} />}
     </div>
   );
 }
