f2bc6e0f5d160896c7739a51d365247fb8720960
khisa-paul <khisapaul@gmail.com>
2025-09-27 10:24:13 +0300

    Update config.js
    update

--- config.js
+++ config.js
@@ -1,5 +1,1 @@
-// src/config.js
-const API_BASE =
-  process.env.REACT_APP_API_BASE || "https://pinkchat-backend-9.onrender.com";
-
-export { API_BASE };
+export const API_BASE = "https://pinkchat-backend-9.onrender.com";
